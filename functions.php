55a6a629dbd1ccc8a6b72271b90b7a72eae213d4
Ajith R N <dev@ajithrn.com>
2020-12-18 01:09:22 +0530

    - theme improvements

--- functions.php
+++ functions.php
@@ -7,17 +7,68 @@
 
 if (!defined('ABSPATH')) die();
 
-function ds_ct_enqueue_parent() { wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); }
+/* -------------------------------------------------------------------------- */
+/*                      enqueue custom scripts and styles                      */
+/* -------------------------------------------------------------------------- */
 
-function ds_ct_loadjs() {
-
-	wp_enqueue_script( 'ds-theme-script', get_stylesheet_directory_uri() . '/ds-script.js',
-        array( 'jquery' )
-    );
-
+function dc_enqueue_parent() { 
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css',false, false ); 
+	wp_enqueue_style( 'dc-custom-style', get_stylesheet_directory_uri() . '/assets/css/custom.css', false, false);
 }
 
-add_action( 'wp_enqueue_scripts', 'ds_ct_enqueue_parent' );
-add_action( 'wp_enqueue_scripts', 'ds_ct_loadjs' );
+function dc_enqueue_scripts() {
+	wp_enqueue_script( 'dc-custom-script', get_stylesheet_directory_uri() . '/assets/scripts/script.js', array( 'jquery' ), false, true );
+}
 
+add_action( 'wp_enqueue_scripts', 'dc_enqueue_parent' );
+add_action( 'wp_enqueue_scripts', 'dc_enqueue_scripts' );
+
+/* -------------------------------------------------------------------------- */
+/*           include divi lazy load for slider and silder bg images           */
+/* -------------------------------------------------------------------------- */
+
+require_once dirname(__FILE__) . '/includes/divi_layzyload.php';
+
+
+/* -------------------------------------------------------------------------- */
+/*                             custom panel edits                             */
+/* -------------------------------------------------------------------------- */
+
+if ( ! function_exists( 'et_load_core_options' ) ) {
+    
+    function et_load_core_options() {
+        $options = require_once( get_stylesheet_directory() . esc_attr( "/panel_options.php" ) );
+    }
+    add_action( 'init', 'et_load_core_options', 999 );
+    
+}
+
+/* -------------------------------------------------------------------------- */
+/*                          custom social media icons                          */
+/* -------------------------------------------------------------------------- */
+if ( ! function_exists( 'et_get_safe_localization' ) ) {
+    
+    function et_get_safe_localization( $string ) {
+    	return wp_kses( $string, et_get_allowed_localization_html_elements() );
+    }    
+}
+
+/* -------------------------------------------------------------------------- */
+/*                           enable shortcode module                          */
+/* -------------------------------------------------------------------------- */
+function showmodule_shortcode($moduleid) {
+	extract(shortcode_atts(array('id' =>'*'),$moduleid)); 
+	return do_shortcode('[et_pb_section global_module="'.$id.'"][/et_pb_section]');
+}
+add_shortcode('showmodule', 'showmodule_shortcode');
+
+/* -------------------------------------------------------------------------- */
+/*                             remove unwanted css                            */
+/* -------------------------------------------------------------------------- */
+add_action( 'wp_print_styles',     'adv_deregister_styles', 100 );
+function adv_deregister_styles()    { 
+	if( !is_user_logged_in() ) {
+		wp_deregister_style( 'dashicons' ); 
+	}
+}
 ?>
---
+++ includes/divi_layzyload.php
@@ -0,0 +1,71 @@
+<?php
+/** ====================================================================================================================
+ * lazyload divi slider bg
+ * https://wordpress.org/plugins/lazy-load-divi-slider-backgrounds/
+ */
+
+// Check if there are any Divi Builder sliders on the page
+function lazy_load_slider_bg_check_if_needed() {    
+	global $post;	
+	$regex = '/\[et_pb_slide[^\]]*background_image[^\]].*?\]/';	
+	if ( preg_match( $regex, $post->post_content ) ) {
+			add_action( 'wp_print_styles', 'lazy_load_slider_bg_add_header_css' );
+			add_action( 'wp_print_footer_scripts', 'lazy_load_slider_bg_add_footer_js' );
+	}
+}
+
+// Print CSS in <head> to hide all slider backgrounds initially
+function lazy_load_slider_bg_add_header_css() {
+	
+	$css_selector = 'html.js #et-boc .et_builder_inner_content .et_pb_slide:not(.et-pb-active-slide):not(.lazy-loaded-slide)';
+	
+	echo '<style id="lazy-load-divi-slider-backgrounds-css">' . $css_selector . ',' . $css_selector . ' > .et_parallax_bg' . '{background-image:none !important}</style>';
+}
+
+// Now we need to add the JS that will load the background images on scroll
+function lazy_load_slider_bg_add_footer_js() {
+?>
+<script id="lazy-load-divi-slider-backgrounds-js">!function(){var a=jQuery.fn.addClass;jQuery.fn.addClass=function(){var e=a.apply(this,arguments);return"et-pb-active-slide"==arguments[0]&&setTimeout(function(){var a=jQuery(".et-pb-active-slide + .et_pb_slide");a.addClass("lazy-loaded-slide"),a.hasClass("et_pb_section_parallax")&&(a=jQuery(".et_parallax_bg",a)).css("background-image",a.css("background-image"))},2e3),e}}();</script>
+<?php
+}
+
+// Add action
+add_action( 'wp', 'lazy_load_slider_bg_check_if_needed', 90 );
+
+
+
+
+
+/** ====================================================================================================================
+ * lazyload divi section bg
+ * https://wordpress.org/plugins/lazy-load-divi-section-backgrounds/
+ */
+
+// Check if there are any Divi Builder sections on the page
+function lazy_load_section_bg_check_if_needed() {    
+	global $post;	
+	$regex = '/\[et_pb_section[^\]]*background_image[^\]].*?\]/';	
+	if ( preg_match( $regex, $post->post_content ) ) {
+			add_action( 'wp_print_styles', 'lazy_load_section_bg_add_header_css' );
+			add_action( 'wp_print_footer_scripts', 'lazy_load_section_bg_add_footer_js' );
+	}
+}
+
+// Print CSS in <head> to hide all section backgrounds initially
+function lazy_load_section_bg_add_header_css() {
+	
+	$css_selector = 'html.js #et-boc .et_builder_inner_content .et_pb_section.et_pb_with_background:not(.lazy-loaded-background)';
+	
+	echo '<style id="lazy-load-divi-section-backgrounds-css">' . $css_selector . ',' . $css_selector . ' > .et_parallax_bg' . '{background-image:none !important}</style>';
+}
+
+// Now we need to add the JS that will load the background images on scroll
+function lazy_load_section_bg_add_footer_js() {	
+	$css_selector = 'html.js #et-boc .et_builder_inner_content .et_pb_section.et_pb_with_background:not(.lazy-loaded-background)';
+?>
+<script id="lazy-load-divi-section-backgrounds-js">function lazyLoadDiviSectionBackgrounds(){jQuery('<?php echo $css_selector ?>').each(function(){var divPos=jQuery(this).offset().top,topOfWindow=jQuery(window).scrollTop();if (divPos<topOfWindow+1000){jQuery(this).addClass('lazy-loaded-background');}})}jQuery(document).ready(function(){lazyLoadDiviSectionBackgrounds()});jQuery(window).scroll(function(){lazyLoadDiviSectionBackgrounds()});jQuery(window).resize(function(){lazyLoadDiviSectionBackgrounds()});</script>
+<?php
+}
+
+// Add action
+add_action( 'wp', 'lazy_load_section_bg_check_if_needed', 90 );
